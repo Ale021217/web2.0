f673cc312232e0a4b0d361d9e18fea874824a6f6
Ale021217 <alejandrojoserj@gmail.com>
2023-07-13 22:05:39 -0600

    Lab05

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './seguridad/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Ruta inicial 
+  { path: 'login', component: LoginComponent },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
